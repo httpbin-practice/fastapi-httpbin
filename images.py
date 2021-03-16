ca1ddb588155e240bd8584d1dad79cdd556a569e
Tacey Wong <xinyong.wang@qq.com>
2021-03-17 01:10:45 +0800

    images completed

--- images.py
+++ images.py
@@ -1,28 +1,37 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter,Request
+from fastapi.responses import FileResponse
 
 images_router = APIRouter(prefix="/image", tags=["images"])
 
+IMAGES_DIR = "images/"
 
 @images_router.get("")
-async def get_image():
-    return "get image"
+async def get_image(request:Request):
+    accept = request.headers.get("accept","image/jpeg")
+    format = accept.split("/")[-1].strip()
+    if format not in ("jpeg","png","svg","webp"):
+        format = "jpeg"
+    return FileResponse(path=os.path.join(IMAGES_DIR,f"img.{format}"))
 
 
 @images_router.get("/jpeg")
 async def get_image():
-    return "get image jpeg"
+    return FileResponse(path=os.path.join(IMAGES_DIR,"img.jpeg"))
 
 
 @images_router.get("/png")
 async def get_image_png():
-    return "get image png"
+    return FileResponse(path=os.path.join(IMAGES_DIR,"img.png"))
 
 
 @images_router.get("/svg")
 async def get_image_svg():
-    return "get image svg"
+    return FileResponse(path=os.path.join(IMAGES_DIR,"img.svg"))
 
 
 @images_router.get("/webp")
 async def get_image_webp():
-    return "get image webp"
+    response = FileResponse(path=os.path.join(IMAGES_DIR,"img.webp"))
+    response.headers["Content-Type"]="image/webp"
+    return response
--- request_inspection.py
+++ request_inspection.py
@@ -1,18 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Request,Header
 
 request_insepct_router = APIRouter(prefix="/request_inspect", tags=["Request Inspection"])
 
 
 @request_insepct_router.get("/headers")
-async def get_request_headers():
-    return "get_request_headers"
+async def get_request_headers(request:Request):
+    return {"headers":request.headers}
 
 
 @request_insepct_router.get("/ip")
-async def get_request_ip():
-    return "get_request_ip"
+async def get_request_ip(request:Request):
+    return {"origin":request.client.host}
 
 
 @request_insepct_router.get("/user-agent")
-async def get_request_user_agent():
-    return "get_request_user_agent"
+async def get_request_user_agent(request:Request):
+    return {"User-Agent":request.headers.get("User-Agent")}
